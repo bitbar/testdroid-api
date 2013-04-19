cd80f5e3136f38e850d119f5de50e16ce6db9344
Lukasz Kajda <lukasz.kajda@bitbar.com>
2013-04-19 15:47:11 +0200

    api v2: label/cluster resources

---
+++ src/main/java/com/testdroid/api/model/APIDeviceProperty.java
@@ -0,0 +1,69 @@
+package com.testdroid.api.model;
+
+import com.testdroid.api.APIEntity;
+import com.testdroid.api.APIException;
+import com.testdroid.api.APIListResource;
+import javax.xml.bind.annotation.XmlRootElement;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/**
+ *
+ * @author kajdus
+ */
+@XmlRootElement
+public class APIDeviceProperty extends APIEntity {
+    private String name;
+    private String displayName;
+    private Integer deviceCount;
+    private Integer creditsPrice;
+
+    public APIDeviceProperty() {}
+
+    public APIDeviceProperty(Long id, String name, String displayName, Integer deviceCount, Integer creditsPrice) {
+        super(id);
+        this.name = name;
+        this.displayName = displayName;
+        this.deviceCount = deviceCount;
+        this.creditsPrice = creditsPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public Integer getDeviceCount() {
+        return deviceCount;
+    }
+
+    public void setDeviceCount(Integer deviceCount) {
+        this.deviceCount = deviceCount;
+    }
+
+    public Integer getCreditsPrice() {
+        return creditsPrice;
+    }
+
+    public void setCreditsPrice(Integer creditsPrice) {
+        this.creditsPrice = creditsPrice;
+    }
+    
+    private String getDevicesURI() { return selfURI + "/devices"; };
+    
+    @JsonIgnore
+    public APIListResource<APIDevice> getDevicesResource() throws APIException {
+        return getListResource(getDevicesURI(), APIDevice.class);
+    }
+        
+}
