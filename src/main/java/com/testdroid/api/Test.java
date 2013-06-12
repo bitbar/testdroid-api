039bf4e6b804f917a9029bef5e913347102b83ff
Lukasz Kajda <lukasz.kajda@bitbar.com>
2013-06-12 10:00:09 +0200

    snapshot to version

--- src/main/java/com/testdroid/api/Test.java
+++ src/main/java/com/testdroid/api/Test.java
@@ -15,17 +15,17 @@ import java.io.File;
 public class Test {
     
     public static void main(String[] args) {
-        APIClient client = new DefaultAPIClient("http://localhost:8080/testdroid-cloud", "admin@localhost", "admin");
+        APIClient client = new DefaultAPIClient("http://devel", "admin@localhost", "admin");
         try {
             APIUser user = client.me();
             System.out.println("SUCCEEDED WITH USER: " + user.getId());
             APIListResource<APIUser> resource = new APIListResource<APIUser>(client, "/users", 0L, 10L, null, 
                     APISort.create(APIUser.class, new SortItem(APISort.Column.USER_EMAIL, APISort.Type.DESC)), APIUser.class);
             System.out.println("USERS COUNT: " + resource.getEntity().getTotal());
-            APIProject p = user.getProject(120731L);
+            APIProject p = user.getProject(104051L);
             System.out.println(p.getName());
             AndroidFiles files = p.getFiles(AndroidFiles.class);
-            files.uploadTest(new File("C:\\Users\\Lukasz\\Desktop\\apks\\MoviesTest2.apk"));
+            files.uploadTest(new File("C:\\Users\\Lukasz\\Desktop\\apks\\MoviesTest.apk"));
             System.out.println(files.getAndroidTest().getOriginalName());
         } catch (APIException ex) {
             ex.printStackTrace();
