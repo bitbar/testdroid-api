7e5d1ba2c4d5b1dc8cb53fead3e37241fe6b7fe8
patrykzabkiewicz <pzabkiewicz@gmail.com>
2014-01-30 13:57:08 +0100

    delete projects

--- src/main/java/com/testdroid/api/sample/CreateProjectSample.java
+++ src/main/java/com/testdroid/api/sample/CreateProjectSample.java
@@ -13,45 +13,20 @@ import com.testdroid.api.sample.util.Common;
  */
 public class CreateProjectSample {
 
-    public static final APIClient CLIENT = Common.createApiClient();
+    public static final APIClient CLIENT = Common.createApiClient("http://testdroid-db","zabkiewi","opek987123");
     
     public static void main(String[] args) {
         APIUser me;
         APIProject project;
         APIProject project1;
-        APIProject project2;
         try {
             // Get authenticated user
             me = CLIENT.me();
             
             // Create proejct with selected type
-            project = me.createProject(APIProject.Type.ANDROID);
-            
-            // Create project with selected type and specified name
-            project1 = me.createProject(APIProject.Type.ANDROID, String.format("Another %s", project.getName()));
-            
-            // After project is created, update project
-            project.setName(String.format("Different %s", project.getName()));
-            project.setDescription("Description of project");
-            project.update();
-            
-            // Cannot create project with the same name
-            try {
-                project2 = me.createProject(APIProject.Type.ANDROID, project.getName());
-            } catch(APIException apiex) {
-                System.err.println(apiex.getMessage());
+            for(int i=0; i<1000; i++) {
+                me.createProject(APIProject.Type.ANDROID);
             }
-            
-            // Cannot update project name to existing project name
-            try {
-                project.setName(project1.getName());
-                project.update();
-            } catch(APIException apiex) {
-                System.err.println(apiex.getMessage());
-            }
-            
-            // Deleting project
-            project.delete();
             
         } catch (APIException apie) {
             System.err.println(apie.getMessage());
--- src/main/java/com/testdroid/api/sample/DeleteProjects.java
+++ src/main/java/com/testdroid/api/sample/DeleteProjects.java
@@ -11,32 +11,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA.
  * User: zabkiewi
- * Date: 23.01.14
+ * Date: 08.01.14
  * Time: 14:03
- * To change this template use File | Settings | File Templates.
  */
 public class DeleteProjects {
 
-    public static final APIClient CLIENT = Common.createApiClient("http://testdroid:9080","svc_quickbuild","qU1ckbU1ld");
-    //public static final String HOOK_URL = "some URL";
+    public static APIClient CLIENT;
 
     public static void main(String[] args) {
         try {
             // Get authenticated user
+            if(args.length < 3) {
+                System.out.println("usage: host login password");
+                System.exit(0);
+            }
+
+            CLIENT =  Common.createApiClient( args[0] , args[1], args[2]);
             APIUser me = CLIENT.me();
             List<APIProject> plist = null;
 
-            while( (plist = me.getProjectsResource().getEntity().getData())!=null ) {
+
+            while( (plist = me.getProjectsResource().getEntity().getData())!=null && (plist.size() > 19) ) {
                 for(APIProject p : plist) {
                     p.delete();
-                    System.out.println(p.getId());
+                    System.out.println("Deleted project ID: " + p.getId());
                 }
             }
+
 
         } catch(APIException apie) {
             System.err.println(apie.getMessage());
         }
+
+        System.exit(0);
     }
 }
