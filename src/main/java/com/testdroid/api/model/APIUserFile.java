d810f0cc1a0f19bb196fbf495f90a070059f6c7a
michal.szpruta <michal.szpruta@bitbar.com>
2014-02-07 15:18:46 +0100

    APIUserFile, APIUserFileProperty

---
+++ src/main/java/com/testdroid/api/model/APIUserFile.java
@@ -0,0 +1,92 @@
+package com.testdroid.api.model;
+
+import com.testdroid.api.APIEntity;
+import java.util.Date;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import org.codehaus.jackson.annotate.JsonIgnore;
+
+/**
+ *
+ * @author Michał Szpruta <michal.szpruta@bitbar.com>
+ */
+@XmlRootElement
+public class APIUserFile extends APIEntity {
+
+    @XmlType(namespace = "APIUserFile")
+    public static enum APIFileType {
+
+        ANDROID_APP, ANDROID_TEST, ANDROID_FILE, IOS_APP, IOS_TEST, DATA, SCREENSHOT, CALABASH_TEST, UIAUTOMATOR_TEST, 
+        UNKNOWN
+    }
+    private String name;
+    private Date createTime;
+    private Long size;
+    private APIFileType fileType;
+    private APIUserFileProperty[] fileProperties;
+
+    public APIUserFile() {
+    }
+
+    public APIUserFile(Long id, String name, Date createTime, Long size, APIFileType fileType, 
+            APIUserFileProperty ...fileProperties) {
+        super(id);
+        this.name = name;
+        this.createTime = createTime;
+        this.size = size;
+        this.fileType = fileType;
+        this.fileProperties = fileProperties;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Long getSize() {
+        return size;
+    }
+
+    public void setSize(Long size) {
+        this.size = size;
+    }
+
+    public APIFileType getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(APIFileType fileType) {
+        this.fileType = fileType;
+    }
+
+    public APIUserFileProperty[] getFileProperties() {
+        return fileProperties;
+    }
+
+    public void setFileProperties(APIUserFileProperty[] fileProperties) {
+        this.fileProperties = fileProperties;
+    }
+
+    @Override
+    @JsonIgnore
+    protected <T extends APIEntity> void clone(T from) {
+        APIUserFile apiUserFile = (APIUserFile) from;
+        cloneBase(from);
+        this.name = apiUserFile.name;
+        this.createTime = apiUserFile.createTime;
+        this.size = apiUserFile.size;
+        this.fileType = apiUserFile.fileType;
+        this.fileProperties = apiUserFile.fileProperties;
+    }
+}
