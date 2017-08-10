48432ee5e6626deb4128162d7b4a9d447a72a2a5
michal.szpruta <michal.szpruta@bitbar.com>
2017-08-10 13:27:56 +0200

    Version 2.43 - update

--- src/main/java/com/testdroid/api/model/enums/APIPaymentMethod.java
+++ src/main/java/com/testdroid/api/model/enums/APIPaymentMethod.java
@@ -1,13 +1,28 @@
 package com.testdroid.api.model.enums;
 
+import static java.lang.Boolean.*;
 /**
  * @author Łukasz Kajda <lukasz.kajda@bitbar.com>
  */
 public enum APIPaymentMethod {
     PAYPAL,
-    BRAINTREE,
-    INVOICE,
+    BRAINTREE(TRUE),
+    INVOICE(FALSE),
     PROMOTION,
-    BLUEMIX,
-    AWS
+    BLUEMIX(TRUE),
+    AWS(FALSE);
+
+    private Boolean allowUpdate;
+
+    private APIPaymentMethod(){
+
+    }
+
+    private APIPaymentMethod(Boolean allowUpdate){
+        this.allowUpdate = allowUpdate;
+    }
+
+    public Boolean getAllowUpdate() {
+        return allowUpdate;
+    }
 }
